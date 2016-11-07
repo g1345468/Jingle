172677e2d997c5b24c126557f313d23820892254
g1345468 <bonds-3164@momoironantoka-no-macbook-air.local>
2016-11-07 14:55:35 +0900

    Initial Commit

---
+++ Jingle/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Jingle
+//
+//  Created by Yamada Seisuke on 2016/11/07.
+//  Copyright © 2016年 Ito.inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
