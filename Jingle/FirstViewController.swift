4c768601867d972cd9a0d62c422323ddeb1da4ef
g1345468 <bonds-3164@MomoiroNantoka-no-MacBook-Air.local>
2016-11-08 16:35:48 +0900

    Show artist names.

--- Jingle/FirstViewController.swift
+++ Jingle/FirstViewController.swift
@@ -7,17 +7,191 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, MPMediaPickerControllerDelegate {
+    
+    
+    var player = MPMusicPlayerController()
+    var mediaItem: MPMediaItem!
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet var rateLabels: [UILabel]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        player = MPMusicPlayerController.systemMusicPlayer()
+        
+        // 再生中のItemが変わった時に通知を受け取る
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: #selector(FirstViewController.nowPlayingItemChanged(_:)), name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification, object: player)
+        notificationCenter.addObserver(self, selector: #selector(FirstViewController.nowPlayingItemChanged(_:)), name: MPMusicPlayerControllerPlaybackStateDidChangeNotification, object: player)
+        // 通知の有効化
+        player.beginGeneratingPlaybackNotifications()
+        
+        setPlayButton()
+        
+
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(FirstViewController.willEnterForegroundNotification(_:)), name: UIApplicationWillEnterForegroundNotification, object: nil)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func willEnterForegroundNotification(notification: NSNotification) {
+        setPlayButton()
+    }
+
+    func setPlayButton() {
+        if player.playbackState == MPMusicPlaybackState.Paused {
+            playButton.setTitle("▶︎", forState: UIControlState.Normal)
+        } else if player.playbackState == MPMusicPlaybackState.Playing {
+            playButton.setTitle("■", forState: UIControlState.Normal)
+        }
+        
+    }
+    
+    
+    @IBAction func pick(sender: AnyObject) {
+        // MPMediaPickerControllerのインスタンスを作成
+        let picker = MPMediaPickerController()
+        // ピッカーのデリゲートを設定
+        picker.delegate = self
+        // 複数選択にする。（falseにすると、単数選択になる）
+        picker.allowsPickingMultipleItems = true
+        // ピッカーを表示する
+        presentViewController(picker, animated: true, completion: nil)
+
+    }
+    
+    func mediaPicker(mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        
+        // プレイヤーを止める
+        player.stop()
+        
+        // 選択した曲情報がmediaItemCollectionに入っているので、これをplayerにセット。
+        player.setQueueWithItemCollection(mediaItemCollection)
+        
+        // 選択した曲から最初の曲の情報を表示
+        mediaItem = mediaItemCollection.items.first
+        if mediaItem != nil {
+            updateSongInformationUI()
+        }
+        
+        // ピッカーを閉じ、破棄する
+        dismissViewControllerAnimated(true, completion: nil)
+        
+        player.play()
+        
+    }
+    
+    func mediaPickerDidCancel(mediaPicker: MPMediaPickerController) {
+        // ピッカーを閉じ、破棄する
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func updateSongInformationUI() {
+        
+        // 曲情報表示
+        // (a ?? b は、a != nil ? a! : b を示す演算子です)
+        // (aがnilの場合にはbとなります)
+        artistLabel.text = mediaItem.artist ?? "不明なアーティスト"
+        songLabel.text = mediaItem.title ?? "不明な曲"
+        
+        /*
+        if mediaItem.artist?.characters.count <= 6 {
+            artistLabel2.loadHTMLString("<center>" + mediaItem.artist! + "</center>", baseURL: nil)
+        } else {
+            artistLabel2.loadHTMLString("<marquee scrolldelay=40 truespeed scrollamount=1>" + mediaItem.artist! + "</marquee>", baseURL: nil)
+        }
+ */
+        
+        
+        setRateButton(mediaItem.rating)
+        
+        // アートワーク表示
+        if let artwork = mediaItem.artwork {
+            let image = artwork.imageWithSize(imageView.bounds.size)
+            imageView.image = image
+        } else {
+            // アートワークがないとき
+            // (今回は灰色表示としました)
+            imageView.image = nil
+            imageView.backgroundColor = UIColor.grayColor()
+        }
+        
+    }
+    
+    func nowPlayingItemChanged(notification: NSNotification) {
+        
+        mediaItem = player.nowPlayingItem
+        if mediaItem != nil {
+            updateSongInformationUI()
+        }
+        
+        setPlayButton()
+        
+    }
+    
+    func setRateButton(rate: Int) {
+        for (var i = 0; i < rateLabels.count; i++) {
+            if(i < rate) {
+                rateLabels[i].text = "★"
+            } else {
+                rateLabels[i].text = "・"
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    deinit {
+        // 再生中アイテム変更に対する監視をはずす
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.removeObserver(self, name: MPMusicPlayerControllerNowPlayingItemDidChangeNotification, object: player)
+        // ミュージックプレーヤー通知の無効化
+        player.endGeneratingPlaybackNotifications()
+    }
+
+
+    @IBAction func pushPlayPause(sender: AnyObject) {
+        if player.playbackState == MPMusicPlaybackState.Playing {
+            player.pause()
+            playButton.setTitle("▶︎", forState: UIControlState.Normal)
+        } else {
+            player.play()
+            playButton.setTitle("■", forState: UIControlState.Normal)
+        }
+    }
+    
+    @IBAction func pushPrevious(sender: AnyObject) {
+        if player.currentPlaybackTime < 3.0 {
+            player.skipToPreviousItem()
+        } else {
+            player.skipToBeginning()
+        }
+    }
+    
+    
+    @IBAction func pushNext(sender: AnyObject) {
+        player.skipToNextItem()
     }
 
 
--- Jingle/SecondViewController.swift
+++ Jingle/SecondViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var playingView: UIView!
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var artistTable: UITableView!
+    
+    var artistNames = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        /*
+        let gerture = UITapGestureRecognizer(target:self, action: #selector(SecondViewController.showPlaying(_:)))
+        nowPlaying.addGestureRecognizer(<#T##gestureRecognizer: UIGestureRecognizer##UIGestureRecognizer#>)
+         */
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(SecondViewController.viewTap(_:)))
+        playingView.addGestureRecognizer(tap)
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(SecondViewController.viewSwipe(_:)))
+        playingView.addGestureRecognizer(swipe)
+        
+        self.view.bringSubviewToFront(playingView)
+
+        let query = MPMediaQuery.artistsQuery()
+        for collection in query.collections! {
+            let artistName = collection.representativeItem!.artist ?? "不明"
+            artistNames.append(artistName)
+        }
+
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func viewTap(sender: UITapGestureRecognizer) {
+        
+        let nextView = self.storyboard?.instantiateViewControllerWithIdentifier("test")
+        self.presentViewController(nextView!, animated: true, completion: nil)
+ 
+    }
+    
+    func viewSwipe(sender: UISwipeGestureRecognizer) {
+        let nextView = self.storyboard?.instantiateViewControllerWithIdentifier("artist")
+        self.presentViewController(nextView!, animated: true, completion: nil)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return artistNames.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        cell.textLabel!.text = artistNames[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    
+    /*
+    func showPlaying(gestureRecognizer: UITapGestureRecognizer) {
+        self.presentedViewController(FirstViewController, animated: true, completion: nil)
+    }
+ */
 
-
+    
 }
 
