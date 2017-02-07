cfcf2d5eda4ad818771a4466611b814d905b67f1
g1345468 <bonds-3164@ass.local>
2017-02-07 15:36:54 +0900

    Modify rating buttons.

---
+++ Jingle/PlaylistViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlaylistViewController.swift
+//  Jingle
+//
+//  Created by Yamada Seisuke on 2016/12/24.
+//  Copyright © 2016年 Ito.inc. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class PlaylistViewController: UIViewController {
+    
+    var playlistNames = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+ 
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+ 
+}
